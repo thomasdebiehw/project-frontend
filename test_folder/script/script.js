ac952a9b4f066ba6b7a265076aff6eeb60e1bf4a
Thomas Debie <thomas.debie@student.howest.be>
2019-05-29 00:20:24 +0200

    test: add database test

--- test_folder/script/script.js
+++ test_folder/script/script.js
@@ -5,11 +5,17 @@ const showTemperature = function (jsonObject) {
     console.log(jsonObject);
     document.querySelector('#temperature').innerHTML= `De temperatuur is ${jsonObject.temperature} graden Celcius.`
   }
+  const showComponents = function (jsonObject) {
+    console.log(jsonObject);
+  }
 //#endregion
 //#region ***********  Data Access ***********
 // get_______
 const getTemperature = function () {
     handleData(`http://${IP}/api/v1/sensors/temperature`, showTemperature);
+}
+const getComponents = function () {
+    handleData(`http://${IP}/api/v1/components`, showComponents);
 }
 //#endregion
 //#region ***********  INIT / DOMContentLoaded ***********
@@ -18,6 +24,11 @@ const init = function () {
     domButton.addEventListener('click', function () {
         console.log('gedrukt')
         getTemperature();
+    });
+    domButtonC = document.querySelector('#buttoncomponents');
+    domButtonC.addEventListener('click', function () {
+        console.log('gedrukt op components')
+        getComponents();
     });
 };
 
