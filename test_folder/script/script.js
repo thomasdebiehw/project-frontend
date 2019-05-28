67096efceb0cc72f9284713b8b804c5596a26ef2
Thomas Debie <thomas.debie@student.howest.be>
2019-05-28 19:48:07 +0200

    test: get temperature from backend

--- test_folder/script/script.js
+++ test_folder/script/script.js
@@ -1,10 +1,25 @@
+const IP = '169.254.10.1:5000';
+//#region ***********  Callback - HTML Generation (After select) ***********
+// show________
+const showTemperature = function (jsonObject) {
+    console.log(jsonObject);
+    document.querySelector('#temperature').innerHTML= `De temperatuur is ${jsonObject.temperature} graden Celcius.`
+  }
+//#endregion
+//#region ***********  Data Access ***********
+// get_______
+const getTemperature = function () {
+    handleData(`http://${IP}/api/v1/sensors/temperature`, showTemperature);
+}
+//#endregion
 //#region ***********  INIT / DOMContentLoaded ***********
 const init = function () {
     domButton = document.querySelector('#button');
     domButton.addEventListener('click', function () {
         console.log('gedrukt')
-      });
-  };
-  
-  document.addEventListener('DOMContentLoaded', init);
-  //#endregion
+        getTemperature();
+    });
+};
+
+document.addEventListener('DOMContentLoaded', init);
+//#endregion
