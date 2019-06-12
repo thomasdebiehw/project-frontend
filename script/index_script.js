195561a44c207c0f390ecdb81e9b294995bc9494
Thomas Debie <thomas.debie@student.howest.be>
2019-06-12 18:35:35 +0200

    allow controlling the alarm from the webinterface

--- script/index_script.js
+++ script/index_script.js
@@ -1,14 +1,14 @@
 const IP = '169.254.10.1:5000';
 const socket = io.connect(IP);
 let domAlarmStatus, domHeatingStatus, domCurrentTemp, domSetTemp
-//#region ***********  Callback - HTML Generation (After select) ***********
+//#region ***********  Callback - HTML Generation (After select) or on socket event ***********
 // show________
-const showTemperature = function (jsonObject) {
-    console.log(jsonObject);
-    document.querySelector('#temperature').innerHTML= `De temperatuur is ${jsonObject.temperature} graden Celcius.`
-  }
-  const showComponents = function (jsonObject) {
-    console.log(jsonObject);
+const showIndexData = function (data) {
+    console.log(data)
+    domHeatingStatus.innerHTML = data.heating_status;
+    domAlarmStatus.innerHTML = data.alarm_status;
+    domCurrentTemp.innerHTML = data.current_temperature;
+    domSetTemp.innerHTML = data.set_temperature;
   }
 //#endregion
 //#region ***********  Data Access ***********
@@ -22,16 +22,15 @@ const getComponents = function () {
 //#endregion
 //#region ***********  INIT / DOMContentLoaded ***********
 const init = function () {
-    domAlarmStatus = document.getElementById("alarm-status");
-    domHeatingStatus = document.getElementById("heating-status")
-    domCurrentTemp = document.getElementById("current-temperature");
-    domSetTemp = document.getElementById("set-temperature");
-    socket.on("index_emit", function(data) {
-        console.log(data)
-        domHeatingStatus.innerHTML = data.heating_status;
-        domAlarmStatus.innerHTML = data.alarm_status;
-        domCurrentTemp.innerHTML = data.current_temperature;
-        domSetTemp.innerHTML = data.set_temperature;
+    domAlarmStatus = document.getElementById('alarm-status');
+    domHeatingStatus = document.getElementById('heating-status');
+    domCurrentTemp = document.getElementById('current-temperature');
+    domSetTemp = document.getElementById('set-temperature');
+    domAlarmStatus.addEventListener('click', function(){
+        socket.emit('toggle_alarm');
+    })
+    socket.on('index_emit', function(data) {
+        showIndexData(data);
 
     });
 };
