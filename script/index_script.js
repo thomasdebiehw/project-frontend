3dde38f9b3f0452d759b12122ba60496d5e20eb4
Thomas Debie <thomas.debie@student.howest.be>
2019-06-13 00:53:22 +0200

    begin of showing events

--- script/index_script.js
+++ script/index_script.js
@@ -1,6 +1,6 @@
 const IP = '169.254.10.1:5000';
 const socket = io.connect(IP);
-let domAlarmStatus, domHeatingStatus, domCurrentTemp, domSetTemp, domSetTempDisp
+let domAlarmStatus, domHeatingStatus, domAlarmRaisedEvents, domCurrentTemp, domSetTemp, domSetTempDisp
 //#region ***********  Callback - HTML Generation (After select) or on socket event ***********
 // show________
 const showIndexData = function (data) {
@@ -9,18 +9,27 @@ const showIndexData = function (data) {
     domAlarmStatus.innerHTML = data.alarm_status;
     domCurrentTemp.innerHTML = data.current_temperature;
     domSetTemp.innerHTML = data.set_temperature;
-  }
-  showSetTempDisplay = function (){
-      domSetTempDisp.innerHTML = `<p><form>
+};
+const showSetTempDisplay = function () {
+    domSetTempDisp.innerHTML = `<p><form>
       <input type="number" name="temperature" min="1" max="100" step="0.5" id="temp-val">
       <input type="button" id="send-temp" value="Set temperature">
       </form></p>`;
-      document.getElementById('send-temp').addEventListener('click', function(){
-          socket.emit('change-temp', document.getElementById('temp-val').value);
-          domSetTempDisp.innerHTML = '';
-      });
+    document.getElementById('send-temp').addEventListener('click', function () {
+        socket.emit('change-temp', document.getElementById('temp-val').value);
+        domSetTempDisp.innerHTML = '';
+    });
+};
+const showNewAlarmRaisedEvents = function(data) {
+    console.log(data);
+    domAlarmRaisedEvents.innerHTML = `<p>`;
+    data.forEach(element => {
+        domAlarmRaisedEvents.innerHTML += `${element[0]} ${element[1]} ${element[2]} ${element[3]} ${element[4]} ${element[5]} <br>`;
+    });
+    domAlarmRaisedEvents.innerHTML += `</p>`;
 
-  }
+
+}
 //#endregion
 //#region ***********  Data Access ***********
 // get_______
@@ -35,17 +44,22 @@ const getComponents = function () {
 const init = function () {
     domAlarmStatus = document.getElementById('alarm-status');
     domHeatingStatus = document.getElementById('heating-status');
+    domAlarmRaisedEvents = document.getElementById('alarm-raised-events');
     domCurrentTemp = document.getElementById('current-temperature');
     domSetTemp = document.getElementById('set-temperature');
     domSetTempDisp = document.getElementById('change-temperature');
-    domAlarmStatus.addEventListener('click', function(){
+    domAlarmStatus.addEventListener('click', function () {
         socket.emit('toggle_alarm');
     })
-    domSetTemp.addEventListener('click', function(){
+    domSetTemp.addEventListener('click', function () {
         showSetTempDisplay()
     });
-    socket.on('index_emit', function(data) {
+    socket.on('index_emit', function (data) {
         showIndexData(data);
+
+    });
+    socket.on('new_alarm_raised_events', function (data) {
+        showNewAlarmRaisedEvents(data);
 
     });
 };
