c98ef9dfe7632f97608e1829f8644acc42777c1d
Thomas Debie <thomas.debie@student.howest.be>
2019-06-13 21:18:07 +0200

    allow changing alarm timeouts

--- script/sensor_script.js
+++ script/sensor_script.js
@@ -1,6 +1,6 @@
 const IP = '169.254.10.1:5000';
 const socket = io.connect(IP);
-let domSensors
+let domSensors, domHeating, domWalkin, domWalkout
 //#region ***********  Callback - HTML Generation (After select) or on socket event ***********
 // show________
 
@@ -14,6 +14,26 @@ let domSensors
 //#region ***********  INIT / DOMContentLoaded ***********
 const init = function () {
     console.log("loaded")
+    domSensors = document.getElementById('sensors');
+    domHeating = document.getElementById('heating');
+    domWalkin = document.getElementById('walkin');
+    domWalkin.innerHTML = `<p><form>Walk-in timeout: 
+    <input type="number" name="timeout" min="1" max="100" step="1" id="walkin-val" value="5"> seconds 
+    <input type="button" id="send-walkin" value="Save">
+    </form></p>`;
+    document.getElementById('send-walkin').addEventListener('click', function () {
+        socket.emit('change-walkin', document.getElementById('walkin-val').value);
+        domWalkin.innerHTML += ' Saved!';
+    });
+    domWalkout = document.getElementById('walkout');
+    domWalkout.innerHTML = `<p><form>Walk-out timeout: 
+    <input type="number" name="timeout" min="1" max="100" step="1" id="walkout-val" value="5"> seconds 
+    <input type="button" id="send-walkout" value="Save">
+    </form></p>`;
+    document.getElementById('send-walkout').addEventListener('click', function () {
+        socket.emit('change-walkout', document.getElementById('walkout-val').value);
+        domWalkout.innerHTML += ' Saved!';
+    });
 };
 
 
